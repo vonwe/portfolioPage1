33ac20ab8ea3f1955be04dfa8f81c585c5e91952
vonwe <vonwe@pvamu.edu>
2025-07-10 23:25:20 -0500

    Clear npm cache, and attempt 3

--- portfolio/src/app/Components/projects/projects.component.ts
+++ portfolio/src/app/Components/projects/projects.component.ts
@@ -15,25 +15,25 @@ export class ProjectsComponent {
     {
       title: 'Autonomous IR Sensor Vehicle',
       description: 'Developed a Python algorithm that allows a vehicle equipped with infrared sensors to follow a defined track autonomously.',
-      image: './assets/finishedVehicle.png',
+      image: 'assets/finishedVehicle.png',
       tags: ['Python', 'Robotics', 'IoT']
     },
     {
       title: 'Ultrasonic Obstacle Avoidance',
       description: 'Built a system using ultrasonic sensors to detect nearby obstacles and automatically steer the vehicle away to avoid collisions.',
-      image: './assets/ultrasonicSensor.png',
+      image: 'assets/ultrasonicSensor.png',
       tags: ['Python', 'Robotics', 'IoT']
     },
     {
       title: 'Computer Vision ArUco UAV',
       description: 'Developed a computer vision system that identifies ArUco markers, classifies one as friendly, and deploys a water-based payload upon others.',
-      image: './assets/computerVision.png',
+      image: 'assets/computerVision.png',
       tags: ['Python', 'OpenCV']
     },
     {
       title: 'Lyrics Search Web App',
       description: 'Full-stack web application using Angular, Azure Data Studio, and C#.NET to search songs via Genius API, display lyrics, and highlight referenced lyrics from other media sources.',
-      image: './assets/fullStackWebApp.png',
+      image: 'assets/fullStackWebApp.png',
       tags: ['Angular', 'C#.NET', 'SQL', 'Azure']
     }
   ];
